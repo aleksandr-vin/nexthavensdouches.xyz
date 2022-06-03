45bef87a4e304d488457f6dbd8b139f19b2913e2
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2022-06-03 11:06:14 +0200

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# A Gatsby starter written in TypeScript made into static page site for [nexthavensdouches.xyz](https://nexthavensdouches.xyz)
-
-## **Developing.**
-
-    ```shell
-    npm run develop
-    ```
--- src/utils/typography.js
+++
@@ -1,7 +0,0 @@
-import Typography from "typography";
-import sternGroveTheme from "typography-theme-stern-grove";
-
-sternGroveTheme.baseFontSize = "30px";
-const typography = new Typography(sternGroveTheme);
-
-export default typography;
